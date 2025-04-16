70d323a3233e79d00b9f32a4a839de568da74b2e
Jesper Quorning <jesper.quorning@gmail.com>
2025-04-16 08:29:21 +0200

    --help and --version on command line

--- source/hostarm.adb
+++ source/hostarm.adb
@@ -1,9 +1,11 @@
 
+with Ada.Command_Line;
 with Ada.Strings.Fixed;
 with Ada.Text_IO;
 
 with AWS.Net;
 
+with Hostarm_Config;
 with HostARM_Configuration;
 with HostARM_Server;
 with HostARM_Tipue;
@@ -16,6 +18,22 @@ procedure HostARM is
    package Config renames HostARM_Configuration;
    use Ada.Text_IO, Ada.Strings;
 begin
+   declare
+      use Ada.Command_Line;
+   begin
+      if Argument_Count = 1 and then Argument (1) = "--version" then
+         Put_Line ("HostARM version " & Hostarm_Config.Crate_Version);
+         return;
+      end if;
+
+      if Argument_Count = 1 and then Argument (1) = "--help" then
+         Put_Line
+           ("Start HostARM and then access (Annotated) Ada Reference " &
+            "Manual in the web browser address htts://localhost:2778/.");
+         return;
+      end if;
+   end;
+
    HostARM_Tipue.Build_Content (Config.ARM_2012);
    HostARM_Tipue.Build_Content (Config.ARM_2022);
    HostARM_Tipue.Build_Content (Config.AARM_202Y);
@@ -24,6 +42,7 @@ begin
    Put_Line ("HostARM: Accessible on URL: https://localhost:"
              & Fixed.Trim (Config.Default_Port'Image, Side => Left)
              & "/");
+   Put_Line ("HostARM: Press 'Q' to quit.");
 
    HostARM_Server.Wait;
    Put_Line ("HostARM: Shutting down");
@@ -33,6 +52,6 @@ begin
 exception
    when AWS.Net.Socket_Error =>
       Put_Line (Standard_Error,
-                "HostARM: Socket in use. Try waiting 10 seconds and "
+                "HostARM: Socket in use. Try waiting 2 minutes and "
                 & "then try again");
 end HostARM;
--- source/hostarm_tools.adb
+++ source/hostarm_tools.adb
@@ -1,5 +1,4 @@
 
-with Ada.Characters.Latin_1;
 with Ada.Strings.Fixed;
 with Ada.Text_IO;
 
