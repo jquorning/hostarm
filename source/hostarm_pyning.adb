83c2ab53f32793012811c8fa3d478f08b3850165
Jesper Quorning <jesper.quorning@gmail.com>
2026-02-28 23:33:16 +0100

    Add shortcut icon

--- source/hostarm_pyning.adb
+++ source/hostarm_pyning.adb
@@ -298,7 +298,10 @@ package body HostARM_Pyning is
       Match : constant String := "</head>";
 
       Repl : constant String :=
-        "<link rel='icon' href='/assets/images/2778.svg'>" & CRLF &
+        "<link rel='icon' type='image/svg+xml' href='/assets/images/2778.svg'>" &
+        CRLF &
+        "<link rel='shortcut icon' type='image/svg+xml' href='/assets/images/2778.svg'>" &
+        CRLF &
         "<link rel='stylesheet' href='/assets/css/arm.css'>" & CRLF &
         "<link rel='stylesheet' href='/assets/css/hostarm.css'>";
    begin
